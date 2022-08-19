2169a08f5ca828ab13494f057864d714a8365196
Ardavan Eskandari <77455553+ardavan8102@users.noreply.github.com>
2022-08-19 21:04:30 +0430

    3.10 Py

--- python.py
+++ python.py
@@ -1,6 +1,16 @@
 from colorama import Fore, init
 init()
 
+# Colors
+yellow = Fore.LIGHTYELLOW_EX
+green = Fore.LIGHTGREEN_EX
+red = Fore.LIGHTRED_EX
+white = Fore.WHITE
+purple = Fore.MAGENTA
+
+# Text Styles
+line_seprator = purple + "* * * * * * * * * * * * *" + white
+
 # Main Food Class
 class Food():
     def __init__(self, foodType):
@@ -9,57 +19,94 @@ class Food():
 
 class Pizza(Food):
     def __init__(self, Type, size):
-        super().__init__("Fast Food")
+        super().__init__("Fast Foods")
         self.size = size
 
-        if size == "big":
+        if size.lower() == "big":
             self.size = "8 Slice"
-        elif size == "small":
+        elif size.lower() == "small":
             self.size = "6 Slice"
         else:
             self.size = "6 Slice (By Default)"
 
-        self.price = "245 T"
+        self.price = "150 T"
         self.Type = Type
 
 class Kabab(Food):
     def __init__(self, Type, sikh):
-        super().__init__("Kabab & Joojeh")
+        super().__init__("Rice Foods")
         self.sikh = sikh
         self.size = f"1 Boshqab Berenj & {self.sikh} Sikh Kabab"
-        self.price = "325 T"
+
+        kabab_money = sikh * 30 # 30 T For Each Skewer
+        rice_money = 110
+        food_money = kabab_money + rice_money # Total Money
+
+        self.price = str(food_money) + " T"
         self.Type = Type
 
 
-# Food Variables
-food1 = Pizza(
-    Type = "Peperoni",
-    size = "small"
-)
 
-food2 = Kabab(
-    Type = "Koobideh", 
-    sikh = 3
-)
+food = ""
 
+# Main Loop
+while True:
+    customer_welcome = input(red + "SYSTEM: " + yellow +"Welcome Dear, Do You Want Food? (y/n) " + white)
 
-# Colors
-yellow = Fore.LIGHTYELLOW_EX
-green = Fore.LIGHTGREEN_EX
-red = Fore.LIGHTRED_EX
-white = Fore.WHITE
+    # If User Decline Order Request :
+    if customer_welcome == "n":
+        print(line_seprator) # Line Seprator (* * * * * * * * * *)
+        print(red + "SYSTEM: " + green + "See You Next Time ❤️")
+        break
 
+    # If User Accept Order Request :
+    elif customer_welcome == "y": 
 
-print(red + "* * * * * * * * * * * * *" + white)
+        print(line_seprator) # Line Sepator
 
-print(
-    yellow + "Food : " + green + f"{food1.Type} Pizza \n" + yellow + "Gheymat :" + green + f" {food1.price}\n" + yellow + "Size Pizza :" + green + f" {food1.size}\n" + yellow + "Category : " + green + f"{food1.foodType}" + white
-)
+        customer_food_type = input(red + "SYSTEM: " + yellow + "What Kind Of Food Do You Want? " + white)
 
-print(red + "* * * * * * * * * * * * *" + white)
+        if customer_food_type.lower() == "pizza":
+            
+            print(line_seprator)
+            pizza_kind = input(red + "SYSTEM: " + yellow + "What Kind Of Pizza You Want? " + white)
 
-print(
-    yellow + "Food : " + green + f"{food2.Type} Kabab \n" + yellow + "Gheymat : " + green + f"{food2.price} \n" + yellow + "Shamele : " + green + f"{food2.size}\n" + yellow + "Category : " + green + f"{food2.foodType}" + white
-)
+            print(line_seprator)
+            pizza_size = input(red + "SYSTEM: " + yellow + "What Size Of Pizza You Want? " + white)
 
-print(red + "* * * * * * * * * * * * *" + white)
+            food = Pizza(
+                Type = pizza_kind, 
+                size = pizza_size
+            )
+
+            print(line_seprator)
+
+            print(red + "SYSTEM: " + green + f"Order Confirmed !\nFood Type : {food.Type}\nFood Price : {food.price}\nFood Size : {food.size}" + white)
+
+            print(line_seprator)
+
+        elif customer_food_type.lower() == "kabab":
+
+            print(line_seprator)
+            kabab_kind = input(red + "SYSTEM: " + yellow + "What Kind Of Kabab You Want? " + white)
+
+            print(line_seprator)
+            kabab_sikh = int(input(red + "SYSTEM: " + yellow + "How Many Skewers Of Kabab You Want? " + white))
+
+            food = Kabab(
+                Type = kabab_kind, 
+                sikh = kabab_sikh
+            )
+
+            print(line_seprator)
+
+            print(red + "SYSTEM: " + green + f"Order Confirmed !\nFood Type : {food.Type}\nFood Price : {food.price}\nSkewers : {food.sikh} Skewers Kabab" + white)
+
+            print(line_seprator)
+
+        else:
+            print(red + "SYSTEM: " + green + "Unknow Type !" + white)
+            print(red + "SYSTEM: " + green + "'Pizza' OR 'Kabab' As Food !" + white)
+            continue
+
+        
